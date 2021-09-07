4e5e5a048cf757489680f1142a41b1f6c001f53a
developer-suhag <developersuhag@gmail.com>
2021-09-07 20:18:07 +0600

    fixed error

---
+++ shopping-cart/app.js
@@ -0,0 +1,54 @@
+// increase and decrease quantity and price
+function updateQuantity(product, price, isPlus) {
+    const productField = document.getElementById(product + '-number');
+    let productQuantity = productField.value;
+    if (isPlus == true) {
+        productQuantity = parseInt(productQuantity) + 1;
+    } else if (productQuantity > 0) {
+        productQuantity = parseInt(productQuantity) - 1;
+    }
+    productField.value = productQuantity;
+    const totalPrice = document.getElementById(product + '-total')
+    totalPrice.innerText = productQuantity * price;
+    // update total
+    updateTotal();
+};
+
+// update total
+function getInput(product) {
+    const productField = document.getElementById(product + '-number').value;
+    const productQuantity = parseInt(productField);
+    return productQuantity;
+}
+
+function updateTotal() {
+    const mobileTotal = getInput('mobile') * 1219;
+    const caseTotal = getInput('case') * 59;
+    const subTotal = mobileTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // update total on the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
+// Handle Mobile increase and decrease events 
+// increase 
+document.getElementById('mobile-plus').addEventListener('click', function () {
+    updateQuantity('mobile', 1219, true);
+});
+// decrease 
+document.getElementById('mobile-minus').addEventListener('click', function () {
+    updateQuantity('mobile', 1219, false);
+});
+
+// handle case increase and decrease events
+// increase
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateQuantity('case', 59, true)
+});
+// decrease 
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateQuantity('case', 59, false);
+})
